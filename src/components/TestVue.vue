2f90401ae11483c53811fbe9c1b571735526eb5e
Bevo1d996 <Bevo1d@163.com>
2024-07-16 17:24:19 +0800

    第一次提交

---
+++ src/components/TestVue.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!-- 简易账户密码登录界面 -->
+    <el-form
+      :model="ruleForm2"
+      label-position="left"
+      label-width="0px"
+      class="demo-ruleForm login-container"
+    >
+      <h3 class="title">系统登录</h3>
+      <el-form-item prop="account">
+        <el-input
+          type="text"
+          v-model="ruleForm2.account"
+          auto-complete="off"
+          placeholder="账号"
+        ></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script setup>
+
+</script>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import App from "./App.vue";
+import router from "./router";
+import { createApp } from "vue";
+import store from "core-js/internals/shared-store";
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(store).use(router).use(ElementPlus).mount("#app");
